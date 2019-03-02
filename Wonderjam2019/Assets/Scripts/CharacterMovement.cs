99bd8e098754b968f3f549e93a17b2bc5dc232af
CantyCanadian <anthony-_-duquette@hotmail.com>
2019-03-02 17:08:11 -0500

    Added prompt display and basic test map.

---
+++ Wonderjam2019/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public int CharacterControllerID = 0;
+    public float MovementSpeed = 2.0f;
+    public float MovementLimit = 2.0f;
+
+    private CharacterController m_CharacterController;
+
+    void Start()
+    {
+        m_CharacterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("JAM_LTRIG_HOR_1");
+        float vertical = Input.GetAxis("JAM_LTRIG_VERT_1");
+
+        m_CharacterController.Move(new Vector3(horizontal * Time.deltaTime * MovementSpeed, 0.0f, -vertical * Time.deltaTime * MovementSpeed));
+
+        //Debug.Log(horizontal);
+        //if (horizontal != 0.0f && Mathf.Abs((transform.localPosition + (Vector3.left * horizontal * Time.deltaTime * MovementSpeed)).magnitude) <= MovementLimit)
+        //{
+        //    transform.position += Vector3.left * horizontal * Time.deltaTime * MovementSpeed;
+        //}
+    }
+}
---
+++ Wonderjam2019/Assets/Scripts/DrinkFountain.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrinkFountain : MonoBehaviour
+{
+    public GameObject ButtonPrompt;
+
+    void Start()
+    {
+        ButtonPrompt.SetActive(false);
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ButtonPrompt.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ButtonPrompt.SetActive(false);
+        }
+    }
+}
